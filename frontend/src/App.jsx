44499719349de600a2c140e45f4d67c22f3c4606
vaibhav <vaibhavning@gmail.com>
2024-08-27 12:00:38 +0530

    update navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
     return (
         <div className="w-full h-screen p-4">
             <nav className="flex justify-between bg-gray-800 p-4">
-                <div className="text-white">Line Chart with Zoom and Data Set Options</div>
+                <div className="text-white">Pie chart to Demonstrate Intensity , Likelihood and Relevance of Energy sectors</div>
             </nav>
             <PieChart/>
         </div>
